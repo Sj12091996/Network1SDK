06d90d5cec8dcf32e73b0decdae3bcb2048ccdc2
Saurabh <sourabhjaiswal@thoughtwin.com>
2024-05-17 13:07:59 +0530

    removed platforms: [         .iOS(.v15),         .macOS(.v12),         .watchOS(.v8)     ],

--- Package.swift
+++ Package.swift
@@ -5,11 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "Network1SDK",
-    platforms: [
-        .iOS(.v15),
-        .macOS(.v12),
-        .watchOS(.v8)
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
